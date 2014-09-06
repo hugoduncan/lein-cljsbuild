5a8085c4e44167e693ff64fd2decc3d6555bff88
Hugo Duncan <hugo@hugoduncan.org>
2014-09-06 10:19:40 -0400

    Set project version

--- plugin/project.clj
+++ plugin/project.clj
@@ -1,4 +1,4 @@
-(defproject lein-cljsbuild "1.0.3"
+(defproject lein-cljsbuild "1.0.4-SNAPSHOT"
   :description "ClojureScript Autobuilder Plugin"
   :url "http://github.com/emezeske/lein-cljsbuild"
   :license
